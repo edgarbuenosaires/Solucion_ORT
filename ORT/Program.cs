d08a83f1a938ff9e9f9aa9f6a87053a1d721e684
EDGARDO GABRIEL AGUIRRE <31602634@comunidadort.edu.ar>
2020-05-24 09:29:42 -0300

    Carga de la solucion

---
+++ ORT/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ORT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
